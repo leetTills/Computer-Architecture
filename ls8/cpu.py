a6ef72cada6acc2fb7b016d3b311d0fee8cf76f4
Tyler <tylerwesleyquinn@gmail.com>
2020-02-27 23:31:10 -0500

    mvp day 2 and 3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,9 +2,14 @@
 
 import sys
 
+SP = 7
+
+MUL = 0b10100010
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
+PUSH = 0b01000101
+POP = 0b01000110
 
 class CPU:
     """Main CPU class."""
@@ -14,6 +19,48 @@ class CPU:
         self.reg = [0] * 8
         self.ram = [0] * 256   
         self.pc = 0
+        self.branch_table = {
+            LDI: self.LDI_op,
+            PRN: self.PRN_op,
+            MUL: self.MUL_op,
+            HLT: self.HLT_op,
+            PUSH: self.PUSH_op,
+            POP: self.POP_op
+        }
+
+    def LDI_op(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+        self.pc += 3
+
+    def PRN_op(self, operand_a, operand_b):
+        print(self.reg[operand_a])
+        self.pc += 2
+
+    def MUL_op(self, operand_a, operand_b):
+        self.reg[operand_a] = self.reg[operand_a] * self.reg[operand_b]
+        self.pc += 3
+
+    def HLT_op(self, operand_a, operand_b):
+        sys.exit(0)
+
+    def PUSH_op(self, operand_a, operand_b):
+        self.push(self.reg[operand_a])
+        self.pc += 2
+
+    def POP_op(self, operand_a, operand_b):
+        self.reg[operand_a] = self.pop()
+        self.pc += 2
+
+    def push(self, value):
+        self.reg[SP] -= 1
+        self.ram_write(self.reg[SP], value)
+        print(f"PUSH reg[SP]: {self.reg[SP]}")
+
+    def pop(self):
+        value = self.ram_read(self.reg[SP])
+        self.reg[SP] += 1
+        print(f"POP reg[SP]: {self.reg[SP]}")
+        return value
 
     def ram_read(self, mar):
         mdr = self.ram[mar]
@@ -28,21 +75,17 @@ class CPU:
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        with open(sys.argv[1]) as f:
+            for line in f:
+                cSplit = line.split("#")
+                num = cSplit[0].strip()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                if num == "":
+                    continue
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                instruction = int(num, 2)
+                self.ram[address] = instruction
+                address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -81,18 +124,8 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            #HLT basically exits the program
-            if op == HLT:
-                sys.exit(0)
-
-            #LDI sets the value of the register, to another value
-            elif op == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            #PRN prints the value of the register at whatever given index
-            elif op == PRN:
-                print(self.reg[operand_a])
-                self.pc += 2
+            if op in self.branch_table:
+                self.branch_table[op](operand_a, operand_b)
             else:
                 print("Unrecognized operation.")
                 sys.exit(1)
